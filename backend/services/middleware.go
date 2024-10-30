617e109808acb93ca242284bcfabb8b84e02a0e2
Will Trojniak <william.trojniak@gmail.com>
2024-10-30 15:37:56 -0400

    Update CORS header

--- backend/services/middleware.go
+++ backend/services/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func CORSMiddleware(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, OPTIONS, DELETE")
