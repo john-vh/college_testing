01d0a97ba92096196e298f0864eea0bda4c429e3
Will <william.trojniak@gmail.com>
2024-12-04 13:14:42 -0500

    Fix login and approve urls

--- frontend/src/components/AccountInfo.tsx
+++ frontend/src/components/AccountInfo.tsx
@@ -5,44 +5,46 @@ import { LandingNavbar } from "../components/LandingNavbar.tsx";
 import { useIsFounder } from "../hooks/useBusinessInfo.ts";
 import { Role } from "./InfoPage.tsx";
 import { useLogout } from "../hooks/useLogout.ts";
+import { useNavigate } from "react-router-dom";
 
 export const AccountInfo = () => {
 
-    const data = useAccountInfo();
-    const role = useGetRole();
-    const logout = useLogout();
+  const data = useAccountInfo();
+  const role = useGetRole();
+  const logout = useLogout();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logout();
-        window.location.href = "http://127.0.0.1:3000";
-    }
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  }
 
 
 
-    if (data != null) {
-        return (
-            <div className="content">
-                <Card interactive={true} >
-                    <H3>Account Information</H3>
-                    <FormGroup label="Name"
-                        labelFor="name" >
-                        <InputGroup id="name" defaultValue={data.name} />
-                    </FormGroup>
-                    <FormGroup label="Email"
-                        labelFor="email" >
-                        <InputGroup id="email" defaultValue={data.email} />
-                    </FormGroup>
-                    <FormGroup label="Roles"
-                        labelFor="roles" >
-                        <Checkbox checked={true}>User</Checkbox>
-                        <Checkbox checked={role === Role.Founder || role === Role.Admin}>Founder</Checkbox>
-                        <Checkbox checked={role === Role.Admin}>Admin</Checkbox>
-                    </FormGroup>
-                    <Button onClick={() => handleLogout()}>Logout</Button>
-                </Card>
-            </div>
-        );
-    }
+  if (data != null) {
+    return (
+      <div className="content">
+        <Card interactive={true} >
+          <H3>Account Information</H3>
+          <FormGroup label="Name"
+            labelFor="name" >
+            <InputGroup id="name" defaultValue={data.name} />
+          </FormGroup>
+          <FormGroup label="Email"
+            labelFor="email" >
+            <InputGroup id="email" defaultValue={data.email} />
+          </FormGroup>
+          <FormGroup label="Roles"
+            labelFor="roles" >
+            <Checkbox checked={true}>User</Checkbox>
+            <Checkbox checked={role === Role.Founder || role === Role.Admin}>Founder</Checkbox>
+            <Checkbox checked={role === Role.Admin}>Admin</Checkbox>
+          </FormGroup>
+          <Button onClick={() => handleLogout()}>Logout</Button>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default AccountInfo;
--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -1,16 +1,16 @@
 import { useCallback } from 'react';
 
 export function useLogout() {
-    const logout = useCallback(async () => {
-        try {
-            const response = await fetch(`http://127.0.0.1:8080/auth/logout`, { method: "POST", mode: "cors", credentials: 'include' });
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  const logout = useCallback(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/logout`, { method: "POST", mode: "cors", credentials: 'include' });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
-    }, []);
-    return logout;
+  }, []);
+  return logout;
 }
